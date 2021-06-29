12502370984efc1e674c5a45c7fdde3e19959c46
ElderGr <gbielrs@gmail.com>
2021-06-29 19:10:52 -0300

    feat [backend]: added migration to create table in database and added Groups entity

---
+++ backend/src/database/migrations/1625003078661-CreateGroups.ts
@@ -0,0 +1,49 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export default class CreateGroups1625003078661 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'groups',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                    default: 'uuid_generate_v4()',
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                },
+                {
+                    name: 'description',
+                    type: 'varchar',
+                },
+                {
+                    name: 'latitude',
+                    type: 'decimal',
+                },
+                {
+                    name: 'longitude',
+                    type: 'decimal',
+                },
+                {
+                    name: 'created_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+                {
+                    name: 'updated_at',
+                    type: 'timestamp',
+                    default: 'now()',
+                },
+            ]
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('groups')
+    }
+}
---
+++ backend/src/entities/Groups.ts
@@ -0,0 +1,27 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('groups')
+class Groups {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    latitude: number;
+
+    @Column()
+    longitude: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+    
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default Groups
