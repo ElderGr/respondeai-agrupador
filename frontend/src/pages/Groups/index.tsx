2d4b8a0f0313a1b7596cdb100ef656d802c60372
ElderGr <gbielrs@gmail.com>
2021-07-02 21:01:58 -0300

    feat [frontend]: Added form to add group on map

--- frontend/src/pages/Groups/index.tsx
+++ frontend/src/pages/Groups/index.tsx
@@ -1,17 +1,30 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React, { useState, useEffect, Fragment, ChangeEvent, FormEvent } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Circle, Tooltip } from 'react-leaflet'
 import ClipLoader from 'react-spinners/ClipLoader'
 import { FiPlus } from 'react-icons/fi';
 
 import GroupsModel from '../../entities/GroupsModel';
 import api from '../../services/api'
+import * as Yup from 'yup'
 
 import './styles.css'
+import getValidationErrors from '../../utils/getValidationError';
+
+const formInitialValues = {
+    name: '',
+    link: '',
+    description: ''
+}
 
 const Groups: React.FC = () => {
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0,0])
     const [groups, setGroups] = useState<GroupsModel[]>([])
     const [loading, setLoading] = useState(false)
+    const [formData, setFormData] = useState(formInitialValues)
+
+    const [errors, setErros] = useState(formInitialValues)
+
+    const [formVisible, setFormVisible] = useState(false)
 
     useEffect(() => {
         setLoading(true)
@@ -31,6 +44,55 @@ const Groups: React.FC = () => {
         setLoading(false)
     }, [])
 
+    const handleFormVisibility = () => {
+        setFormVisible(!formVisible)
+    }
+
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        })
+    }
+    
+    const handleFormSubmit = async (event: FormEvent) => {
+        event.preventDefault()
+        
+        setErros(formInitialValues)
+
+        try{
+            const shema = Yup.object().shape({
+                name: Yup.string()
+                  .required('Nome do grupo obrigatório'),
+                link: Yup.string().required('Link para o grupo obrigatório'),
+                description: Yup.string().required('Descrição obrigatória'),
+            });
+    
+            await shema.validate(formData, {
+                abortEarly: false,
+            });
+
+            const { data } = await api.post('groups', {
+                ...formData,
+                latitude: initialPosition[0],
+                longitude: initialPosition[1]
+            })
+
+            setGroups([ ...groups, data ])
+            setFormData(formInitialValues)
+        }catch(err){
+            if(err instanceof Yup.ValidationError){
+                const errors = getValidationErrors(err);
+                
+                setErros({
+                    name: errors.name,
+                    description: errors.description,
+                    link: errors.link
+                })
+            }
+        }
+    }
+
     return (
         <div id='page-groups'>
             {initialPosition[0] === 0 ? (
@@ -48,7 +110,7 @@ const Groups: React.FC = () => {
                         />
                         <Circle center={initialPosition} radius={1000} />
                         {groups.map(group => (
-                            <Marker position={[group.latitude, group.longitude]}>
+                            <Marker key={group.id} position={[group.latitude, group.longitude]}>
                                 <Tooltip>{group.name}</Tooltip>
                                 <Popup >
                                     <div>
@@ -65,7 +127,46 @@ const Groups: React.FC = () => {
                     </MapContainer>
                 </Fragment>
             )}
-            <button type='button'>
+
+            {formVisible && (
+                <form onSubmit={handleFormSubmit}>
+                    <h1>Adicionar novo Grupo</h1>
+                    <div className='field'>
+                        <label>Nome</label>
+                        <input
+                            onChange={handleInputChange}
+                            type="text"
+                            id='name'
+                            name='name'
+                        />
+                        <span>{errors.name}</span>
+                    </div>
+                    <div className='field'>
+                        <label>Link</label>
+                        <input
+                            onChange={handleInputChange}
+                            type="text"
+                            id='link'
+                            name='link'
+                        />
+                        <span>{errors.link}</span>
+                    </div>
+                    <div className='field'>
+                        <label>Descrição</label>
+                        <input
+                            onChange={handleInputChange}
+                            type="text"
+                            id='description'
+                            name='description'
+                        />
+                        <span>{errors.description}</span>
+                    </div>
+
+                    <button type='submit'>Cadastrar</button>
+                </form>
+            )}
+
+            <button onClick={handleFormVisibility} type='button'>
                 <FiPlus size={20} />
                 <span>Adicionar novo Grupo</span>
             </button>
